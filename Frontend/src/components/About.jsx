69a70da7eb5d20c72aa12bf25dbf7efda45922b0
Saket Abhishek <saketabhishek11@gmail.com>
2025-08-04 00:14:47 +0530

    Fix: Removed submodule and re-added Frontend as normal directory

---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <section id="about">
+      <h1 className="lfh1">About Us</h1>
+      <p className="about-desc">
+        Welcome to <span className="brand-name">Lost and Found</span> — your trusted online hub for reuniting people with their cherished belongings.
+        <br /><br />
+        We know the pain of losing something you care about — whether it’s a beloved pet, a precious memento, or a must-have gadget. That’s why we created a space where hope meets help.
+        <br /><br />
+        Our mission? Simple. To connect finders and seekers in one supportive, people-powered platform. Together, we turn moments of loss into stories of reunion.
+        <br /><br />
+        Because sometimes, all it takes is one post, one click, or one kind stranger to make things whole again.
+      </p>
+      <footer>
+        <p>&copy; 2025 Lost and Found</p>
+        <p>
+          Designed and built with <span role="img" aria-label="love">❤️</span> by <a href="#"><span>Saket</span></a>
+        </p>
+      </footer>
+    </section>
+  );
+}
+
+export default About;
